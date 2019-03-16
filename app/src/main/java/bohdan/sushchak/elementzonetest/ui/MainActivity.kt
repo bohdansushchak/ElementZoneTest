73a2654389db9bdeddf9b5f15b52b028ef9c9ec3
Bohdan Sushchak <bogdansushak@gmail.com>
2019-03-16 23:33:59 +0200

    MainActivity fixed set visibility for toolbar and set title

--- app/src/main/java/bohdan/sushchak/elementzonetest/ui/MainActivity.kt
+++ app/src/main/java/bohdan/sushchak/elementzonetest/ui/MainActivity.kt
@@ -1,22 +1,48 @@
 package bohdan.sushchak.elementzonetest.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
 import bohdan.sushchak.elementzonetest.R
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+const val IS_HIDE_ACTION_BAR_KEY = "isHideActionBar"
+
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
 
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+
+        actionBar?.customView = toolbar
+        actionBar?.isHideOnContentScrollEnabled
+        actionBar?.hide()
 
         navController = Navigation.findNavController(this@MainActivity, R.id.nav_host_fragment)
+        navController.addOnDestinationChangedListener(this@MainActivity)
+    }
 
+    override fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {
+        val title = destination.label
+        if (!TextUtils.isEmpty(title))
+            tvToolbarTitle.text = title
+
+        val isHideActionBar = destination.arguments[IS_HIDE_ACTION_BAR_KEY]?.defaultValue as Boolean?
+
+        if (isHideActionBar == true)
+            toolbar.visibility = View.GONE
+        else
+            toolbar.visibility = View.VISIBLE
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        navController.removeOnDestinationChangedListener(this@MainActivity)
     }
 }
